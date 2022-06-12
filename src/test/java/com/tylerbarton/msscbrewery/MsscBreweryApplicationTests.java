30bfc47f09a7f39eac2510862a399375f5736034
tylerbarton <tylervbarton@gmail.com>
2022-06-12 11:17:31 -0500

    Initial Commit

---
+++ src/test/java/com/tylerbarton/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylerbarton.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
